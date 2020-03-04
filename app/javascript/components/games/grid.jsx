4fbe8fec4f66d83262743ef4c9d385871b30fd7e
Shiva Bhusal <hotline.shiva@gmail.com>
2020-03-04 10:50:52 +0545

    Makes GameList to fetch data from API

--- app/javascript/components/games/grid.jsx
+++ app/javascript/components/games/grid.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import('./grid.scss')
-const Grid = ({cells = new Array(16).fill('a')})=>(
+const Grid = ({game })=>(
     <div className="boggle-grid m-auto">
         {
-            cells.map(char => <div>{char}</div>)
+            game.grid.map(char => <div>{char}</div>)
         }
     </div>
 )
--- app/views/api/v1/games/show.jbuilder
+++ app/views/api/v1/games/show.jbuilder
@@ -1,6 +1,6 @@
 
     json.id @game.id
-    json.grid @game.grid
+    json.grid @game.grid.flatten
     json.noOfTimesPlayed @game.play_count
     json.noOfPlayers @game.player_count
     json.topScore @game.top_score
