565965837137117c8c5c0c28477b2a5d8535cc51
Shiva Bhusal <hotline.shiva@gmail.com>
2020-03-05 20:46:09 +0545

    Adds Home page

--- app/javascript/components/header.jsx
+++ app/javascript/components/header.jsx
@@ -11,7 +11,7 @@ const Header = () => (
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
-                    <Link className="nav-link" to="#">Home <span className="sr-only">(current)</span></Link>
+                    <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
                 </li>
                 <li className="nav-item">
                     <Link className="nav-link" to="/dashboard">Dashboard</Link>
---
+++ app/javascript/components/home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = ()=>(
+    <section className="text-center">
+        <h1>Mind Boggle</h1>
+        <hr/>
+        <img src="https://usercontent2.hubstatic.com/14702929_f520.jpg" alt=""/>
+        </section>
+)
+
+export default Home;
